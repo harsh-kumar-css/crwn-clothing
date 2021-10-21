b916c3e46dc326bc9275f35a6b5c17da172fca2e
harsh-kumar-css <harshkrgautamm@gmail.com>
2021-10-21 23:22:17 +0530

    added withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => <div>HatsPage</div>;
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
